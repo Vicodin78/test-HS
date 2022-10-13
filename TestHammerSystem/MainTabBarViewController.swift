43816fd1de3055299246039d9c1207568d1a8a98
vicodin <jazper78@mail.ru>
2022-10-13 17:30:52 +0200

    Added pictures to TabBar

--- TestHammerSystem/MainTabBarViewController.swift
+++ TestHammerSystem/MainTabBarViewController.swift
@@ -8,9 +8,31 @@
 import UIKit
 
 class MainTabBarViewController: UITabBarController {
+    
+    let menuVC = MenuViewController()
+    let contactsVC = ContactsViewController()
+    let profileVC = ProfileViewController()
+    let basketVC = BasketViewController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupControllers()
+    }
+    
+    private func setupControllers() {
+        menuVC.tabBarItem.title = "Меню"
+        menuVC.tabBarItem.image = UIImage(named: "menu")
         
+        contactsVC.tabBarItem.title = "Контакты"
+        contactsVC.tabBarItem.image = UIImage(named: "contacts")
+        
+        profileVC.tabBarItem.title = "Профиль"
+        profileVC.tabBarItem.image = UIImage(named: "profile")
+        
+        basketVC.tabBarItem.title = "Корзина"
+        basketVC.tabBarItem.image = UIImage(named: "basket")
+        
+        viewControllers = [menuVC, contactsVC, profileVC, basketVC]
     }
 }
+
